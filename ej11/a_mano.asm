9f5ae9d632e657c081001146d1205871fbb2c748
smesarubio <smesarubio@itba.edu.ar>
2024-04-26 18:24:15 -0300

    fibonacci corregido

--- ej11/a_mano.asm
+++ ej11/a_mano.asm
@@ -1,21 +1,36 @@
-global fibonacci
+section .text
+global fibo
 
-fibonacci:
-push ebp
-mov ebp, esp
+fibo:
+    push ebp
+    mov ebp, esp
+    push ebx
+    push ecx
+    push edx
 
+    mov ebx, [ebp+8]    ; me guardo el n en ebx
+    cmp ebx, 2          ; me fijo si es menor a 2
+    jl .end
 
-mov ebx, [ebp + 8]
-cmp ebx, 2
-jl .end
-mov ecx, ebx
-mov edx, ebx
-sub ebx, 1
-push ecx
-call fibonacci
+    mov ecx, ebx  ; me copio mi n en ecx
+    sub ecx, 1  ; n-1
+    sub ebx, 2  ; n-2
+    
+    push ebx    ; en ebx tengo n-2
+    call fibo
+    add esp, 4      ; limpio arg
+    mov edx, eax    ; guardo el primer fibo en edx
+    
+    push ecx    ; en ecx tengo n-1
+    call fibo
+    add esp, 4      ; limpio arg
+    add edx, eax    ; sumo mi nuevo fibo en el registro anterior (edx)
+    mov ebx, edx    ; copio el resultado de la suma de los fibos en ebx asi despues salta al .end
 
-
-
-.end 
-leave
-ret
+    .end:
+    mov eax, ebx    ; guardo en la direccion de retorno el resultado del fibonacci
+    pop edx
+    pop ecx
+    pop ebx
+    leave
+    ret
